0b6433008bc3271768d5f60f58b0aa15a5a5ee0e
Mohammad-Ali-Rauf <mohammadhadirauf@gmail.com>
2022-07-06 10:34:51 +0500

    -

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Import Styles
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Hello World with TypeScript</h1>
+    </div>
+  )
+}
+
+export default App;
